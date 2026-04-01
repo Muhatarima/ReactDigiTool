836816bc1f44ba1a4701050d0f7353cb5544cee9
Muhatarima <muhatarima@gmail.com>
2026-04-01 13:25:05 +0600

    usestate in pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -4,13 +4,39 @@ function Pricing(){
         const [activePlan , setActivePlan]=useState('Pro');
 
 return(
-    <div>
-        <div className={activePlan==="Starter" ?"bg-purple-600" : "bg-white"} onClick={()=>setActivePlan("Starter")} >Starter</div>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className={activePlan==="Starter" ?"bg-purple-600 text-white shadow-sm p-6 rounded-xl" : "bg-white  shadow-sm p-6 rounded-xl   "} onClick={()=>setActivePlan("Starter")} >
 
 
 
-        <div onClick={()=>setActivePlan("Pro")} >Pro</div>
-        <div onClick={()=>setActivePlan("Enterprise")}  >Enterprise</div>
+
+            <h3 className="text-2xl font-bold">Starter</h3>
+  <p className="text-4xl font-bold mt-2">$0<small>/months</small></p>
+  <p className="mt-3">Perfect for getting started</p>
+  <button className="mt-6 btn rounded-full">Get Started Free</button>
+        </div>
+
+
+
+        <div   className={activePlan==="Pro"?"bg-purple-600  text-white shadow-sm p-6 rounded-xl":"bg-white  shadow-sm p-6 rounded-xl  "}  onClick={()=>setActivePlan("Pro")} >
+
+
+<h3 className="text-2xl font-bold">Pro</h3>
+  <p className="text-4xl font-bold mt-2">$29</p>
+  <p className="mt-3">For growing teams and creators.</p>
+  <button className="mt-6 btn rounded-full">Start Pro Trial</button>
+
+        </div>
+        <div className={activePlan==="Enterprise"? "bg-purple-600   text-white shadow-sm p-6 rounded-xl  ":"bg-white shadow-sm p-6 rounded-xl "} onClick={()=>setActivePlan("Enterprise")}  >
+
+
+            
+  <h3 className="text-2xl font-bold">Enterprise</h3>
+  <p className="text-4xl font-bold mt-2">$29</p>
+  
+  <p className="mt-3">For teams and businesses</p>
+  <button className="mt-6 btn rounded-full">Contact Sales</button>
+        </div>
 
     </div>
 
