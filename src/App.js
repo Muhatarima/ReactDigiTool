b94d58da986aea1eaf42fbe9bcecd235dfae92d0
Muhatarima <muhatarima@gmail.com>
2026-04-01 22:03:04 +0600

    app error fixed

--- src/App.js
+++ src/App.js
@@ -8,30 +8,47 @@ import Pricing from './components/Pricing';
 import Steps from "./components/Steps";
 
 function App() {
-  let cart=[];
-  const [cart,setCart]=useState([]);
-  function handleAddToCart(product) {
-  setCart([...cart, product]);
-};
-    function removeCart(product){
-        
-    }
-
+  const [cart, setCart] = useState([]);
   const [activeTab, setActiveTab] = useState('products');
+
+  function handleAddToCart(product) {
+    setCart([...cart, product]);
+  }
+
+  function removeCart(id) {
+    const remaining = cart.filter((item) => item.id !== id);
+    setCart(remaining);
+  }
 
   return (
     <div>
       <Navbar cartCount={cart.length} />
       <Banner />
 
-      <div className='flex items-center justify-center gap-1'  >
-        <button  className='btn text-center bg-purple-600 py-3 px-10 border-2 hover:bg-white hover:text-purple-600 text-white rounded-full border-none '  onClick={() => setActiveTab('products')}>Products</button>
-      <button  className='btn text-center btn-outline hover:bg-purple-600 hover:text-white py-3 px-10 border-2 border-purple-500 text-purple-600 rounded-full' onClick={() => setActiveTab('cart')}>{cart.length === 0 ? 'Cart' : `Cart(${cart.length})`}</button>
+      <div className='flex items-center justify-center gap-1'>
+        <button
+          className='btn text-center bg-purple-600 py-3 px-10 border-2 hover:bg-white hover:text-purple-600 text-white rounded-full border-none'
+          onClick={() => setActiveTab('products')}
+        >
+          Products
+        </button>
+
+        <button
+          className='btn text-center btn-outline hover:bg-purple-600 hover:text-white py-3 px-10 border-2 border-purple-500 text-purple-600 rounded-full'
+          onClick={() => setActiveTab('cart')}
+        >
+          {cart.length === 0 ? 'Cart' : `Cart(${cart.length})`}
+        </button>
       </div>
 
-      {activeTab === 'products' ? <Products /> : <Cart/>}
+      {activeTab === 'products' ? (
+        <Products handleAddToCart={handleAddToCart} />
+      ) : (
+        <Cart cart={cart} removeCart={removeCart} />
+      )}
+
       <Steps />
-      <Pricing/>
+      <Pricing />
       <Footer />
     </div>
   );
